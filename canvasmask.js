b24cbbfe0773ab26141e20673b606e2fd59530fd
tomasz stryjewski <stryjewski.t@gmail.com>
2013-06-17 11:22:49 +0300

    Update canvasmask.js
    - removed unused vars
    - used Array.forEach instead of while loop, thus moving the variables into iterator scope
    - reduced overall amount of vars

--- canvasmask.js
+++ canvasmask.js
@@ -2,33 +2,27 @@
 
   var imagecanvas = document.createElement('canvas');
   var imagecontext = imagecanvas.getContext('2d');
-  var images = document.querySelectorAll('.mask');
-  var all = images.length;
-  var img = false;
-  var mask = false;
-  var width = 0;
-  var height = 0;
-  var imgpixels = false;
-  var maskpixels = false;
-  var allpixels = false;
 
   /* uncomment do see the canvas to debug
   document.body.appendChild(imagecanvas);
   */
-  window.addEventListener('load', function(ev){
-    while (all--) {
-      img = images[all];
-      mask = document.createElement('img');
+  window.addEventListener('load', function(){
+    [].forEach.call(document.querySelectorAll('.mask'), function(img){
+      var width  = img.offsetWidth;
+      var height = img.offsetHeight;
+
+      var mask = document.createElement('img');
       mask.src = img.getAttribute('data-mask');
-      width = img.offsetWidth;
-      height = img.offsetHeight;
-      imagecanvas.width = width;
+
+      imagecanvas.width  = width;
       imagecanvas.height = height;
+
       imagecontext.drawImage(mask, 0, 0, width, height);
       imagecontext.globalCompositeOperation = 'source-atop';
       imagecontext.drawImage(img, 0, 0);
+
       img.src = imagecanvas.toDataURL();
-    }
+    });
   }, false);
 
 })();
